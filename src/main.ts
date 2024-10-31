c49a16f68a4959f426ab662dbdd0bad3555a9ea9
Nicolas Boullon <boullon.nicolas@gmail.com>
2024-10-31 15:07:14 -0300

    vinculacion con firebase e instalacion de capacitor

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,17 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(IonicModule.forRoot({})),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({"projectId":"triogourmetpps","appId":"1:107493929792:web:143ece4e0ec2319fc880a5","storageBucket":"triogourmetpps.firebasestorage.app","apiKey":"AIzaSyDgy98WJ7ovyGUBE0hNeyTeOSuBdGPCiVE","authDomain":"triogourmetpps.firebaseapp.com","messagingSenderId":"107493929792"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
