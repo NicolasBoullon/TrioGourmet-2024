d0a1644f73d5ab5ffbb53b9e3d1103e4129701bd
MateoSpatola <mateospatola@hotmail.com>
2024-10-31 18:22:09 -0300

    Creación splash screen.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'splash', pathMatch: 'full' },
+  {
+    path: 'splash',
+    loadComponent: () => import('./pages/splash/splash.page').then( m => m.SplashPage)
+  },
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
   },
 ];
---
+++ src/app/pages/splash/splash.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonContent, IonText } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.page.html',
+  styleUrls: ['./splash.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonText]
+})
+export class SplashPage {
+
+  constructor(private _router: Router) {
+    setTimeout(() => {
+      this._router.navigateByUrl('/login', { replaceUrl: true });
+    }, 2000);
+  }
+
+}
