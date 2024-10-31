c9cde40c66108433eaa451b8ac7731c1eb1a54f4
Nicolas Boullon <boullon.nicolas@gmail.com>
2024-10-31 09:50:36 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'trio-gourmet-spatola-boullon-videla',
+  webDir: 'www'
+};
+
+export default config;
