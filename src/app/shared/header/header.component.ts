53cedc3d654e9d6b0282a723416447a0b321c370
MateoSpatola <mateospatola@hotmail.com>
2024-11-01 12:15:25 -0300

    Creación header.

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,63 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {
+  IonHeader,
+  IonButtons,
+  IonBackButton,
+  IonToolbar,
+  IonTitle,
+  IonButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logOutOutline } from 'ionicons/icons';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [
+    IonIcon,
+    IonHeader,
+    IonButtons,
+    IonBackButton,
+    IonToolbar,
+    IonTitle,
+    IonButton,
+    RouterLink
+  ],
+})
+export class HeaderComponent implements OnInit {
+  private _authService = inject(AuthService);
+  private _notificationService = inject(NotificationService);
+
+  @Input() title: string = '';
+  @Input() backButton: string | null = null;
+  @Input() showSignOut: boolean = false;
+
+  constructor() {
+    addIcons({ logOutOutline });
+  }
+
+  ngOnInit() {}
+
+  async signOut() {
+    await this._notificationService.presentLoading('Cerrando sesión...', 2000);
+    try {
+      await this._authService.signOut();
+      this._notificationService.routerLink('/login');
+    } catch (error) {
+      await this._notificationService.presentToast(
+        '¡Error: No se pudo cerrar sesión!',
+        1000,
+        'danger',
+        'middle'
+      );
+    } finally {
+      await this._notificationService.dismissLoading();
+    }
+  }
+}
